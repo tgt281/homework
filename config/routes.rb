99eac1ce1a651e291e12e3dbe38355a8d243ee40
vagrant <vagrant@kellogg-dev-box>
2014-05-11 01:59:06 +0000

    User stories implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ get "/books/list" => "books/list"
 
 get "/dice/roll" => "dice/roll"
 
+# Custom home page
+get "/" => "books#home"
+
+# Books
+get "/books" => 'books#index'
+get "/books/:id" => 'books#show'
+
 end
